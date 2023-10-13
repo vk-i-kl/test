aa77cca4d200cf25878ff3de2f4d139b1e585289
Vika <viktoriiaklymiuk@gmail.com>
2023-10-13 15:04:22 +0300

    made button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [state,setState] = useState(0)
+    const click =()=>setState(prevState => prevState +1)
+    return (
+        <div>
+          Hello world
+            Ти клікнула {state} разів
+            <button onClick={click}>click me</button>
+        </div>
+    );
+};
 
 export default App;
