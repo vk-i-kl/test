e339a59e56036ce25d2294776fc49b27894beecd
Vika <viktoriiaklymiuk@gmail.com>
2023-10-13 15:06:52 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <div>
           Hello world
-            Ти клікнула {state} разів
+            You clicked {state} time
             <button onClick={click}>click me</button>
         </div>
     );
